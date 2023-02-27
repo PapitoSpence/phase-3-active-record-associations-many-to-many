d6459c0368d334af40585b184e4c1fb3b55a4d08
PapitoSpence <PapitoSpence>
2023-02-27 12:10:21 +0300

    commit tree 15071c2517544dee1a23fd62da607e140ca19fb6

---
+++ db/migrate/20230227084929_add_user_id_to_reviews.rb
@@ -0,0 +1,5 @@
+class CreateAddUserIdtoreviews < ActiveRecord::Migration[6.1]
+  def change
+    add_column :reviews, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_113221) do
 
-  create_table "games", force: :cascade do |t|
+ActiveRecord::Schema.define(version: 2022_09_14_163402) do
+
+  create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "genre"
-    t.string "platform"
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer "score"
-    t.string "comment"
-    t.integer "game_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
   end
 
 end
